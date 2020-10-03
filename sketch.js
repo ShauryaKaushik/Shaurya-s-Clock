ef39a2a1b8f14ad636a8094ef80a3091e885c030
ShauryaKaushik <65485931+ShauryaKaushik@users.noreply.github.com>
2020-10-03 20:06:17 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -28,16 +28,19 @@ function draw() {
   stroke("Red");
 
   let ending1 = map(sc,0,60,0,360);
+  
   arc(0,0,300,300,0,ending1);
   
   stroke("Orange");
   
   let ending2 = map(mn,0,60,0,360);
+  
   arc(0,0,280,280,0,ending2);
 
   stroke("Yellow");
 
   let ending3 = map(hr,0,12,0,360);
+  
   arc(0,0,260,260,0,ending3);
   
   push();
